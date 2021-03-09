3f1a481cf07f87142f77bc1be90a0899c58af650
Carter Clark <crtrclark@gmail.com>
2021-03-08 20:57:32 -0600

    changed prompt methods

--- src/ui/UserInterface.java
+++ src/ui/UserInterface.java
@@ -100,7 +100,7 @@ public class UserInterface implements Serializable {
 	 * @param errorMessage - a custom error message in case of invalid input
 	 * @return non-negative integer entered by user
 	 */
-	public static int getInt(String prompt, String errorMessage) {
+	public static int getInteger(String prompt, String errorMessage) {
 		String read = "";
 		int value = 0;
 		boolean error = true;
@@ -346,9 +346,9 @@ public class UserInterface implements Serializable {
 			String name = getString("Enter product's name: ");
 			String id = getString("Enter product's id: ");
 
-			double currentPrice = Double.parseDouble(getString("Enter product's current price: "));
-			int stockOnHand = Integer.parseInt(getString("Enter product's stock on hand: "));
-			int reorderedLevel = Integer.parseInt(getString("Enter product's reorder level: "));
+			double currentPrice = getDouble("Enter product's current price: ", "A valid number was not entered.");
+			int stockOnHand = getInteger("Enter product's stock on hand: ", "A valid integer was not entered.");
+			int reorderedLevel = getInteger("Enter product's reorder level: ", "A valid integer was not entered.");
 
 			Request.instance().setProductName(name);
 			Request.instance().setProductId(id);
@@ -380,7 +380,7 @@ public class UserInterface implements Serializable {
 				System.out.println();
 				// loading request (data transfer logic) with relevant information
 				Request.instance().setProductId(getString("Enter item's product ID:"));
-				Request.instance().setOrderQuantity(getInt("Enter item quantity:", "Not a valid number."));
+				Request.instance().setOrderQuantity(getInteger("Enter item quantity:", "Not a valid number."));
 				// carrying out the addItem of CheckOut class (inner class of GroceryStore) and
 				// returning the corresponding info (result code, result's product fields, and
 				// order quantity, in order to display it
@@ -513,7 +513,7 @@ public class UserInterface implements Serializable {
 		help();
 		do {
 			System.out.println();
-			action = getInt("Enter option number (" + HELP + " for help):", "Not a valid option number.");
+			action = getInteger("Enter option number (" + HELP + " for help):", "Not a valid option number.");
 			System.out.println();
 			switch (action) {
 			case ENROLL_MEMBER:
