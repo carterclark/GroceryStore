0b808625212c9002802b6c53b563f062ac3fc100
Carter Clark <crtrclark@gmail.com>
2021-03-13 19:16:14 -0600

    changed format

--- src/store/tests/AutomatedTester.java
+++ src/store/tests/AutomatedTester.java
@@ -123,9 +123,7 @@ public class AutomatedTester {
 		for (int index : checkedOutProductIndexes) {
 
 			Request.instance().setProductId(productIds[index]);
-
 			Request.instance().setOrderQuantity(checkedOutQuantity);
-
 			Result result = checkOut.addItem(Request.instance());
 			// testing addItem from CheckOut (inner class of GroceryStore
 			assert result.getResultCode() == Result.ACTION_SUCCESSFUL;
