925b7366410ce4fa6d492ec3b12a62e37d4b39fe
Carter Clark <crtrclark@gmail.com>
2021-03-13 19:12:14 -0600

    fixed testing

--- src/store/tests/AutomatedTester.java
+++ src/store/tests/AutomatedTester.java
@@ -37,7 +37,7 @@ public class AutomatedTester {
 	private int orderCount = 20;
 	private String[] orderNumbers = { "O-1", "O-2", "O-3", "O-4", "O-5", "O-6", "O-7", "O-8", "O-9", "O-10", "O-11",
 			"O-12", "O-13", "O-14", "O-15", "O-16", "O-17", "O-18", "O-19", "O-20" };
-	private String checkedOutMemberId = "M-2";
+	private String checkedOutMemberId = "M-4";
 	private int[] checkedOutProductIndexes = { 4, 9, 14, 19 };
 	private int checkedOutQuantity = 10;
 
@@ -107,7 +107,7 @@ public class AutomatedTester {
 		for (int index = 0; index < orderCount; index++) {
 			Request.instance().setOrderId(orderNumbers[index]);
 
-			Result result = GroceryStore.instance().processShipment(Request.instance());
+			Result result = groceryStore.processShipment(Request.instance());
 
 			assert result.getResultCode() == Result.ACTION_SUCCESSFUL;
 			assert result.getOrderId().equalsIgnoreCase(orderNumbers[index]);
@@ -118,18 +118,24 @@ public class AutomatedTester {
 	}
 
 	public void testCheckOut() {
-		CheckOut checkOut = GroceryStore.instance().new CheckOut(checkedOutMemberId);
+
+		CheckOut checkOut = groceryStore.new CheckOut(checkedOutMemberId);
 		for (int index : checkedOutProductIndexes) {
+
 			Request.instance().setProductId(productIds[index]);
+
 			Request.instance().setOrderQuantity(checkedOutQuantity);
+
 			Result result = checkOut.addItem(Request.instance());
 			// testing addItem from CheckOut (inner class of GroceryStore
 			assert result.getResultCode() == Result.ACTION_SUCCESSFUL;
 		}
+
 		Iterator<Result> returnedIterator = checkOut.closeCheckOut();
 		int counter = 0;
 		// testing closeCheckout from CheckOut (returning list of reordered products
 		for (Iterator<Result> iterator = returnedIterator; iterator.hasNext();) {
+
 			Result result = iterator.next();
 			assert result.getProductId().equalsIgnoreCase(productIds[checkedOutProductIndexes[counter]]);
 			assert result.getOrderQuantity() == reorderLevel[checkedOutProductIndexes[counter]] * 2;
@@ -148,7 +154,7 @@ public class AutomatedTester {
 
 		testProcessShipment();
 
-//		testCheckOut();
+		testCheckOut();
 
 //		UserInterface.instance().listMembers();
 //		UserInterface.instance().listProducts();
