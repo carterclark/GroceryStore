574fba8ee9dee43e50dabaf47ab179d4ed50d143
Carter Clark <crtrclark@gmail.com>
2021-03-27 17:35:37 -0500

    Removed Equals and Hash Methods From Member and Product (#46)
    * removed equals and hash methods

--- src/store/entities/Member.java
+++ src/store/entities/Member.java
@@ -117,60 +117,6 @@ public class Member implements Serializable {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((address == null) ? 0 : address.hashCode());
-		result = prime * result + ((dateJoined == null) ? 0 : dateJoined.hashCode());
-		long temp;
-		temp = Double.doubleToLongBits(feePaid);
-		result = prime * result + (int) (temp ^ (temp >>> 32));
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((phoneNumber == null) ? 0 : phoneNumber.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object object) {
-		if (this == object)
-			return true;
-		if (object == null)
-			return false;
-		if (getClass() != object.getClass())
-			return false;
-		Member other = (Member) object;
-		if (address == null) {
-			if (other.address != null)
-				return false;
-		} else if (!address.equals(other.address))
-			return false;
-		if (dateJoined == null) {
-			if (other.dateJoined != null)
-				return false;
-		} else if (!dateJoined.equals(other.dateJoined))
-			return false;
-		if (Double.doubleToLongBits(feePaid) != Double.doubleToLongBits(other.feePaid))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (phoneNumber == null) {
-			if (other.phoneNumber != null)
-				return false;
-		} else if (!phoneNumber.equals(other.phoneNumber))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return "Member name: " + name + "\tMember ID: " + id;
 	}
